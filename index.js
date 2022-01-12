b4a62128bd892e4e90fbd939a5f755d28b377f6a
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-11 17:03:41 -0800

    3 tests passing

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(customerName) {
+    var customerName = customerName.toUpperCase();
+    return customerName;
+}
+console.log(upperCaseCustomerName(customerName));
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function leastFavoriteCustomer() {
+    const leastCustomer = 'bob';
+    function changeLeastFavoriteCustomer() {
+        const leastCustomer= 'actually rick';
+    }
+    const resultFromSecondFunc = secondFunc();
+    return resultFromSecondFunc;
+}
